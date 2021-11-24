35cb8df5ea6022510becbf46f2fe3ab7bca7d57e
sie17025 <45293549+sie17025@users.noreply.github.com>
2021-11-24 17:19:58 -0600

    Add files via upload

--- Andrés Sierra/Código/Matlab/PruebaComunicacion_UDP.m
+++ Andrés Sierra/Código/Matlab/PruebaComunicacion_UDP.m
@@ -22,23 +22,53 @@ string1 = strcat(string1, string(pose1(3)));
 
 
 % ----- DIRECCION IP DEL AGENTE PARA EL ENVIO ------
-address1 = '10.0.0.4';
+% address1 = '10.0.0.4';
+address1 = '192.168.0.9';
 % address2 = '192.168.0.12';
 
 % --------- PUERTO DE COMUNICACION (FIJO) ----------
-port = 2010;  
-% ------------- VARIABLE PARA ENVIAR ---------------
+port = 2010; 
+% echoudp('on', port);  % Si queremos enviar y recibir aquí mismo.
+                        % Comentarlo si queremos intercambiar mensajes con otro 
+
+% -------- VARIABLE PARA SEGUIR ENVIANDO -----------
 continueRunning = true;
-% ---------- CREAR EL SOCKET DE ENVIO --------------
-u = udpport('byte',"IPV4");
+
+% ================ CREAR EL SOCKET DE ENVIO =====================
+% ---------------------- PARA MATLAB 2021 -----------------------
+% u = udpport('byte',"IPV4");
+% ---------------------- PARA MATLAB 2019 -----------------------
+% -- USANDO LOCAL HOST --
+u = udp(address1,port,'Timeout', 20);
+% HOST REMOTO. Colocar la IP del otro lado (esta u otra computadora)
+% u = udp('192.168.1.15', puerto_enviar, 'LocalPort', port, 'Timeout', 20);
+
+fopen(u);
+% Con esto, se termina la recepción hasta detectar el '\n'
+% u.DatagramTerminateMode = 'off';  
 
 % CICLO PARA ENVIAR LA INFORMACION - Siempre que la variable
 % continueRunning sea igual a true, se enviará la información contenida en
 % el string1 a la dirección address1
+
 while(continueRunning)
-    write(u,string1,"char",address1,port);
+%     write(u,string1,"char",address1,port);
 %     write(u,string2,"char",address2,port);
+%% Enviar y recibir
+    % fwrite(u,65:74)     % Envía como enteros
+    % A = fread(u,10)     % Lee como enteros
+    fprintf(u, string1); % Envía como cadenas
+    msg = fscanf(u)     % Lee como cadenas
+
+%     fprintf(u, 'Hasta luego'); % Envía como cadenas
     continueRunning = input('Continue? ');
 end
-data = readline(u);
-disp(data)
+%% Desconectar y limpiar
+% ---- FUNCIONA UNICAMENTE MATLAB 2021 -------
+% data = readline(u);
+% disp(data)
+% --------- FUNCIONA PARA MATLAB 2019 --------
+%echoudp('off')
+fclose(u);
+delete(u)
+clear u
