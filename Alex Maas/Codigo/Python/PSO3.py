38c64006d37c76ce979cba3f2d7173afa3e65c01
AlexDME <45135105+AlexDME@users.noreply.github.com>
2021-12-15 13:34:54 -0600

    Update PSO3.py

--- Alex Maas/Codigo/Python/PSO3.py
+++ Alex Maas/Codigo/Python/PSO3.py
@@ -11,7 +11,7 @@ n_iterations = 100           #No. de iteraciones (100 funciona bien para compara
 n_particles = 100
 target_error = 1e-6
 dt = 0.1                    #Scaler de Velcidad
-funcion = 1                 #(0-Sphere, 1-Himmelblau, 3-Booth, 3-Rosenbrock)
+funcion = 1                 #(0-Sphere, 1-Himmelblau, 2-Booth, 3-Rosenbrock)
 
 # Calculo de Inercia PSO 
 w_min = 0.5
@@ -98,13 +98,22 @@ class Espacio():
         for particle in self.particles:
             plt.plot(particle.position[0], particle.position[1], 'ro')      #Plot particulas (rojo)
         plt.plot(self.gBest_position[0], self.gBest_position[1], 'bo')      #Plot mejor particula (azul)
-        #plt.suptitle("Himmelblau: 100 agentes", 
-        #  fontdict={'family': 'serif', 
-        #            'color' : 'darkblue',
-        #            'weight': 'bold',
-        #            'size': 16})
-        #plt.title("Global best: [ 3.0 2.0]")
-        plt.show()
+        plt.suptitle("Himmelblau:100 agentes Inercia Caótica", 
+         fontdict={'family': 'serif', 
+                    'color' : 'darkblue',
+                    'weight': 'bold',
+                    'size': 16})
+        plt.xlim(-5,5)
+        plt.ylim(-5,5)
+        feature_x = np.linspace(-5,5,100)
+        feature_y = np.linspace(-5,5,100)
+        [X , Y] = np.meshgrid(feature_x, feature_y)
+        #fig, ax = plt.subplots(1,1)
+        Z = pow(X*X + Y -11,2) + pow(X + Y*Y -7,2) 
+        #Z = pow(X, 2) + pow(Y, 2)
+        plt.contour(X, Y, Z, 20)
+        plt.title("Global best: [ 3.000000 2.000000]")
+        plt.show() #Caótica Himmelblau Sphere
 
 search_space = Espacio(1, target_error, n_particles)
 particle_vector = [Particula() for _ in range(search_space.n_particles)]
